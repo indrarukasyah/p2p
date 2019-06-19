71938d848e58dfecef6ce69185460896fc3fe703
pallavi.dogra <pallavi.dogra@hotmail.com>
2019-06-20 00:14:03 +0530

    admin updated

--- p2papp/admin.py
+++ p2papp/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Slider
-
+from django.contrib.admin.views.main import ChangeList
 
 class SliderC(admin.ModelAdmin):
     list_filter = ('layer_one',)
+    list_display = ('layer_one','layerone_span','layer_two')
+
+
 
 
 admin.site.register(Slider,SliderC)
--- p2papp/templatetags/customtags.py
+++ p2papp/templatetags/customtags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template.loader import render_to_string
 from django.urls import resolve
 
 register = template.Library()
@@ -6,4 +7,21 @@ register = template.Library()
 
 @register.simple_tag
 def admin_filter_list(cl, spec):
-    return {'choices': list(spec.choices(cl))}
+    choices = {'choices':list(spec.choices(cl))}
+    return render_to_string('tags_html/admin_filter_list.html',choices)
+
+@register.simple_tag
+def dir_context(context):
+    print(dir(context))
+    return context
+
+@register.simple_tag
+def admin_lists_display(context):
+    data = {'display': context.list_display, 'queryset': context.queryset}
+    return render_to_string('tags_html/admin_lists_display.html', data)
+
+@register.simple_tag
+def show_list_name(query,name):
+    x = str(name)
+    print(query)
+    return True
